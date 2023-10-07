57f5d4ba5e21cc47e9a775635f05b38dd9a0f304
Tomáš Kliner <klinetom@fit.cvut.cz>
2023-10-07 13:47:10 +0200

    kostra

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,30 +1,44 @@
+'use client'
 import Image from 'next/image'
-import { useState } from 'react'
+import {useState} from 'react'
 
 export default function Home() {
-  type Preferences = [
-    {
-      title: string,
-      contains: string,
-    }
-  ]
+    type Preferences = [
+        {
+            title: string,
+            contains: string,
+        }
+    ]
 
-  const [preferences, setPreferences] = useState<Preferences>();
+    const [preferences, setPreferences] = useState<Preferences>();
 
-  function handlePreferenceTitleChange(e, id){
-      setPreferences(last=>{
-          return
+    function handlePreferenceTitleChange(e: any, id: number) {
+        setPreferences(last => {
+            return last.map((preference: any, index: number) => {
+                if (index === id) {
+                    return {...preference, title: e.target.value}
+                } else {
+                    return preference
+                }
+            })
         })
-  }
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {preferences && preferences.map((preference,id) => {
-          return (
-            <input value={preference.title} onChange={(e)=> handlePreferenceTitleChange(e.target.value, id)}>
-            </input>
-          )
-      })}
-       
-    </main>
-  )
+    }
+
+
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            {preferences && preferences.map((preference, id) => (
+                <div>
+                    <input value={preference.title} onChange={(e) => handlePreferenceTitleChange(e.target.value, id)}>
+                    </input>
+                    <input value={preference.contains}
+                           onChange={(e) => handlePreferenceTitleChange(e.target.value, id)}>
+                    </input>
+                    <button onClick={}>remove</button>
+                </div>
+            ))}
+            <button onClick={}>add</button>
+
+        </main>
+    )
 }
