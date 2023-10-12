76953a573f0cff475930a498e08a4623b967f40f
TomasKliner <tomasklinerr@gmail.com>
2023-10-12 11:13:47 +0200

    input handeling

--- app/meal/page.tsx
+++ app/meal/page.tsx
@@ -1,12 +1,23 @@
-import {TextField} from "@mui/material";
+'use client'
+import {TextareaAutosize, TextField, Button} from "@mui/material";
+import {useState} from "react";
 
 
 export default function Meal() {
+    const [userText, setUserText] = useState('')
+    const [systemText, setSystemText] = useState('')
+
     return (
         <div>
-            <h1 className={"text-center text-4xl p-2 font-bold"}>Meal</h1>
-            <div className={"flex justify-center items-center w-full"}>
-                    <TextField id="outlined-basic" label="" variant="outlined" label={"What kind of food would you like ?"} className={"w-1/2"}/>
+            <h1 className={"text-center text-4xl p-4 font-bold"}>Meal</h1>
+            <div className={"flex flex-col gap-4 justify-center items-center w-full p-2 md:w-1/2 mx-auto"}>
+                <TextField id="outlined-basic" variant="outlined" label={"What kind of food would you like ?"}
+                           value={userText} onChange={(e) => setUserText(e.target.value)}
+                           className={"w-full"}/>
+                <TextField id="label" variant="outlined" label={"system message:"}
+                           value={systemText} onChange={(e) => setSystemText(e.target.value)}
+                           className={"w-full"}/>
+                <Button type={"submit"} variant={"outlined"} className={"w-full"}>Submit</Button>
             </div>
         </div>
     )
