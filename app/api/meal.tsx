8dca619dc65b64e3b15c48cd4948d8da73acc5b1
TomasKliner <tomasklinerr@gmail.com>
2023-10-13 23:39:38 +0200

    updated to use static stern website

--- app/api/meal.tsx
+++ app/api/meal.tsx
@@ -1,12 +1,28 @@
 import {NextRequest, NextResponse} from "next/server";
 
 
-
-export default function Meal(request: NextRequest, response: NextResponse){
+export default async function Meal(request: NextRequest, response: NextResponse) {
     if (request.method === "POST") {
-
-    }
-    else {
+        fetch("https://api.openai.com/v1/chat/completions",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    "prompt": "",
+                    "system_prompt": "",
+                    "temperature": 0.9
+                })
+            })
+            .then(res => res.json())
+            .then(data => {
+                    console.log(data)
+                    return NextResponse.next()
+                }
+            )
+            .catch(err => console.log(err))
+    } else {
         return NextResponse.next()
     }
 
