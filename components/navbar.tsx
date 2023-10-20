c82aa28af98aa7b2cbbb9d1044582dca79f1b987
TomasKliner <tomasklinerr@gmail.com>
2023-10-20 10:07:19 +0200

    pages in navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,28 @@
+'use client'
+import {Icon} from "@iconify/react";
+import Link from "next/link";
 
 
+const pages = [
+    {
+        title: 'Homepage',
+        href: '/',
+    },
+    {
+        title: 'Meal',
+        href: '/meal',
+    },
+    {
+        title: 'Preferences',
+        href: '/preferences',
+    },
+    {
+        title: 'Plan',
+        href: '/plan',
+    }
+]
 export default function Navbar() {
-
+    const user = false;
     return (
         <div className="navbar bg-green-100 shadow-lg rounded-b-box">
             <div className="navbar-start">
@@ -15,9 +36,7 @@ export default function Navbar() {
                     </label>
                     <ul tabIndex={0}
                         className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 shadow">
-                        <li><a>Homepage</a></li>
-                        <li><a>Portfolio</a></li>
-                        <li><a>About</a></li>
+                        {pages.map((page, index) => <li><Link href={page.href}>{page.title}</Link></li>)}
                     </ul>
                 </div>
             </div>
@@ -34,11 +53,12 @@ export default function Navbar() {
                 </button>
                 <button className="btn btn-ghost btn-circle">
                     <div className="indicator">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24"
-                             stroke="currentColor">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                                  d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"/>
-                        </svg>
+
+                        {!user &&
+                            <Link href={"/signIn"}>
+                                <Icon icon={"ph:user-circle-fill"} className={"h-8 w-8"}/>
+                            </Link>
+                        }
                         <span className="badge badge-xs badge-primary indicator-item"></span>
                     </div>
                 </button>
