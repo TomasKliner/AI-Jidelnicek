4ecfd964bc94d10ca3152a60ea8965f64a1d45e1
TomasKliner <tomasklinerr@gmail.com>
2023-10-20 09:58:38 +0200

    simple homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,60 +1,29 @@
 'use client'
 import Image from 'next/image'
 import {useState} from 'react'
-import { Icon } from '@iconify/react';
+import {Icon} from '@iconify/react';
+
 export default function Home() {
-    type Preferences = [
-        {
-            title: string,
-            contains: string,
-        }
-    ]
-
-    const [preferences, setPreferences] = useState<Preferences>();
-
-    function handlePreferenceTitleChange(e: any, id: number) {
-        setPreferences(last => {
-            return last.map((preference: any, index: number) => {
-                if (index === id) {
-                    return {...preference, title: e.target.value}
-                } else {
-                    return preference
-                }
-            })
-        })
-    }
-
-
     return (
-        <div>
-            <h1>Preferences</h1>
-            {preferences?.map((preference, index) => {
-                return (
-                    <div className={"flex items-center justify-center gap-1"} key={index}>
-                        <input className={"input input-primary"} placeholder={"food name"} type="text" value={preference.title}
-                               onChange={(e) => handlePreferenceTitleChange(e, index)}/>
-                        <input className={"input input-primary"} placeholder={"suroviny"} type="text" value={preference.contains}/>
-                        <div className={"cursor-pointer btn btn-error"} onClick={()=>{
-                            setPreferences(last => {
-                                return last.filter((preference: any, i: number) => {
-                                    return i !== index
-                                })
-                            })
-                        }}><Icon icon={"ph:x-bold"}/></div>
+        <>
+            <header className={"flex justify-evenly p-8 mt-4"}>
+
+                <div>
+                    <h1 className={"font-black text-4xl"}>Tired trying to figure out, what to eat ?</h1>
+                    <p className={"text-2xl"}>Let AI find your perfect food choice.</p>
+                    <div className={"flex flex-col w-64 gap-2 p-4"}>
+                        <div className={"w-full p-2 bg-indigo-500"}>use Facebook</div>
+                        <div className={"w-full p-2 bg-indigo-500"}>use Google</div>
                     </div>
-                )
-            })}
-            <button onClick={() => {
-                setPreferences(last => {
-                    if (last) {
-                        return [...last, {title: '', contains: ''}]
-                    } else {
-                        return [{title: '', contains: ''}]
-                    }
-                })
-            }}>
-                Add Preference
-            </button>
-        </div>
+                </div>
+                <div className={"w-[35rem] h-[25rem] bg-slate-800 text-center text-white"}>
+                    Image placeholder
+                </div>
+            </header>
+            <section>
+                <h1 className={"text-center text-4xl p-4 font-bold"}>How does it work ?</h1>
+
+            </section>
+        </>
     )
 }
